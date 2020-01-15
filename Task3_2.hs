58a43ff6e2dd02421f7f2d555f99e0437fb3a91f
Igor-Manikhin <imanihin822@gmail.com>
2020-01-15 22:59:15 +0300

    Task3_2 done

--- Task3_2.hs
+++ Task3_2.hs
@@ -12,10 +12,35 @@ import Todo(todo)
 
 data ReverseList a = RNil | RCons (ReverseList a) a
 
+instance (Show a) => Show (ReverseList a) where
+  show xs = "r" ++ (show $ rlistToList xs)
+
+instance (Eq a) => Eq (ReverseList a) where
+  (==) x y = (rlistToList x) == (rlistToList y)
+
+instance (Ord a) => Ord (ReverseList a) where
+  compare x y = compare (rlistToList x) (rlistToList y)
+
+instance Semigroup (ReverseList a) where
+  xs <> ys = listToRList ((rlistToList xs) <> (rlistToList ys))
+
+instance Monoid (ReverseList a) where
+  mempty = RNil
+
+instance Functor ReverseList where
+  fmap f x = listToRList (f <$> (rlistToList x))
+
+
 rlistToList :: ReverseList a -> [a]
-rlistToList = todo
+rlistToList = reverse . rlistToList'
+  where
+    rlistToList' RNil         = []
+    rlistToList' (RCons xs x) = x : rlistToList' xs
 
 listToRList :: [a] -> ReverseList a
-listToRList = todo
+listToRList = listToRList' . reverse
+  where
+    listToRList' [] = RNil
+    listToRList' (x:xs) = RCons (listToRList' xs) x
 
 -- Реализуйте классы Eq, Ord, Show, Monoid, Functor

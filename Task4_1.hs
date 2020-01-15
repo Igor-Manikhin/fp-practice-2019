ed7d1e8840d9e2d4f0aae7e337ca16275fa9bcd9
Igor-Manikhin <imanihin822@gmail.com>
2020-01-15 19:50:49 +0300

    Task4_1 and Task4_2 done

--- Task4_1.hs
+++ Task4_1.hs
@@ -8,6 +8,93 @@ module Task4_1 where
 
 -- Монада над функцией. В качестве входного значения `fun` может быть что угодно
 -- Собственно, почему бы не `String`?
+-- Тогда пусть при оборачивании значения в эту монаду fun представляет 
+-- константную функцию, на любом значении возвращающую само это значение
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor FunMonad where
+  -- fmap :: (a -> b) -> f a -> f b
+  fmap f fm = let g = fun fm in FunMonad $ f . g
+  -- g :: String -> a
+  -- f :: a -> b
+  -- f . g :: String -> b
+  -- FunMonad (f . g) :: FunMonad b
+  {-
+    Проверка законов функторов:
+    1. fmap id x == x
+      fmap id (FunMonad f) = FunMonad $ id . f = FunMonad f --- верно
+    2. fmap (f . g) x = fmap f (fmap g x)
+      fmap (f . g) (FunMonad h) = FunMonad (f . g . h) 
+                                = FunMonad (f . (g . h)) = fmap f (FunMonad (g . h))
+                                = fmap f (fmap g (FunMonad h)) --- верно  
+  -}
+
+instance Applicative FunMonad where
+  pure x = FunMonad (\_ -> x)
+  mf <*> ma = FunMonad (\s -> f s (a s))
+    where f = fun mf
+          a = fun ma
+  {-
+    Проверка аппликативных законов:
+    1. pure id <*> v = v
+      pure id = FunMonad (\s -> a -> a) -- FunMonad g
+      pure id <*> (FunMonad f) = FunMonad (\s -> g s (f s)) = FunMonad (\s -> f s) = FunMonad f
+      --- верно
+    2. pure f <*> pure x = pure (f x)
+      pure f = FunMonad (\s -> \t -> f t) = FunMonad g
+      pure x = FunMonad (\s -> x) = FunMonad y
+      FunMonad g <*> FunMonad y = FunMonad (\s -> g s (y s)) 
+                                = FunMonad (\s -> g s x) 
+                                = FunMonad (\s -> f x) 
+                                = pure (f x)
+                                --- верно
+    3. u <*> pure y = pure ($ y) <*> u
+      FunMonad f <*> FunMonad (\s -> y) = FunMonad (\s -> f s y) --- (*)
+      pure ($ y) = FunMonad (\s -> ($ y)) = FunMonad x
+      pure ($ y) <*> FunMonad f = FunMonad x <*> FunMonad f
+                                = FunMonad (\s -> x s (f s))
+                                = FunMonad (\s -> ($ y) (f s))
+                                = FunMonad (\s -> f s y) --- (**)
+      (*) = (**)
+      --- верно
+
+    
+  -}
+  
+instance Monad FunMonad where
+  return = pure
+  ma >>= mf = mf' <*> ma
+    where mf' = FunMonad $ flip $ fun . mf
+  {-
+    Проверка монадных законов:
+    1. return x >>= mf = mf x
+      return x = pure x
+      pure x >>= mf = mf' <*> pure x
+      mf' = FunMonad $ flip (fun . mf)
+      mf :: a -> FunMonad b
+      fun . mf :: a -> String -> b
+      flip $ fun . mf :: String -> a -> b --- просто раскрыли функцию mf
+      FunMonad $ ... :: FunMonad (a -> b) --- привели функцию mf к виду f (a -> b)
+      FunMonad f <*> pure x = pure ($ x) <*> FunMonad f
+                            = pure ($ x) <*> FunMonad (flip $ fun . mf)
+                            = FunMonad (\s -> ($ x)) <*> FunMonad (flip $ fun . mf)
+                            = FunMonad g <*> ...
+                            = FunMonad (\s -> g s (flip $ fun . mf $ s))
+                            = FunMonad (\s -> ($ x) (flip $ fun . mf $ s))
+                            = FunMonad (fun . mf $ x)
+                            = mf x
+                            --- верно
+    2. ma >>= return = ma
+      ma >>= return = ma >>= pure
+      fun . pure = \b -> \s -> b = const
+      flip const = const'
+      FunMonad g = ma, g = \s -> a
+      FunMonad const' <*> FunMonad g = FunMonad (\s -> const' s (g s)) 
+                                     = FunMonad (\s -> g s) 
+                                     = FunMonad g
+                                     = ma
+      --- верно
+    3. (mv >>= f) >>= g  =  mv >>= (\x -> (f x >>= g))
+  -}
--- Task4_2.hs
+++ Task4_2.hs
@@ -11,3 +11,41 @@ data FourOf a = FourOf a a a a deriving(Show,Eq)
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+
+instance Functor FourOf where
+	fmap f (FourOf a b c d) = FourOf (f a) (f b) (f c) (f d)
+	{-
+		Проверка функторных законов:
+		1. fmap id x
+		fmap id (FourOf a b c d) = FourOf (id a) (id b) (id c) (id d)
+		                         = FourOf a b c d
+		2. fmap (f . g) x = fmap f (fmap g x)
+		fmap (f . g) (FourOf a b c d) = FourOf (f . g $ a) ...
+		                              = FourOf (f (g a)) ...
+		                              = fmap f (fmap g (FourOf a b c d)) 
+	-}
+
+instance Applicative FourOf where
+	pure x = FourOf x x x x
+	(FourOf f1 f2 f3 f4) <*> (FourOf v1 v2 v3 v4) = FourOf (f1 v1) (f2 v2) (f3 v3) (f4 v4)
+
+instance Monad FourOf where
+	return = pure
+	(FourOf x1 x2 x3 x4) >>= mf = FourOf y1 y2 y3 y4
+		where FourOf y1 _ _ _ = mf x1
+		      FourOf _ y2 _ _ = mf x2
+		      FourOf _ _ y3 _ = mf x3
+		      FourOf _ _ _ y4 = mf x4
+
+
+{-
+Проверка монадных законов:
+    1. return x >>= mf = mf x
+    очевидно
+    2. ma >>= return = ma
+    очевидно
+    3. (mv >>= f) >>= g  =  mv >>= (\x -> (f x >>= g))
+    mv >>= f = (FourOf x1 x2 x3 x4) >>= mf = FourOf y1 y2 y3 y4
+    FourOf y1 y2 y3 y4 >>= g = FourOf z1 z2 z3 z4
+    в силу "лёгкой" раскрываемости содерижимого
+-}

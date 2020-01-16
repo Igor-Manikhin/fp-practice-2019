2a6cd9a9ca48cf05f9a9f9b6a8c79c6b880df32d
Igor-Manikhin <imanihin822@gmail.com>
2020-01-16 11:36:10 +0300

    Task5_1 and Task5_2 done

--- Task5_1.hs
+++ Task5_1.hs
@@ -28,16 +28,43 @@ list2dlist lst = list2dlist' DNil lst
 list2dlist' :: DList a -> [a] -> DList a
 list2dlist' _ [] = DNil
 list2dlist' left (h: t) = 
-    let rec = DCons left h (list2dlist' rec t)
-    in rec
+    let u = DCons left h (list2dlist' u t)
+    in u
 
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index DNil _ = error "Index out of bounds"
+index (DCons _ x _) 0 = x
+index (DCons left _ right) n 
+    | n < 0 = index left (abs n - 1)
+    | n > 0 = index right (n-1)
+
+flipList :: DList a -> DList a
+flipList DNil = DNil
+flipList (DCons l x r) = DCons r x l
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt DNil 0 x = DCons DNil x DNil
+insertAt DNil _ _ = error "Index out of bounds"
+insertAt (DCons left x right) n a
+    | n == 1 = let t = DCons left x (insertAt' t a right) in t
+    | n == 0 = let t = DCons DNil a (insertAt' t x right) in t
+    | otherwise =
+        case right of
+            DNil -> error "Index out of bounds"
+            _    -> DCons left x (insertAt right (n - 1) a)
+
+
+insertAt' :: DList a -> a -> DList a -> DList a
+insertAt' l x DNil = DCons l x DNil
+insertAt' l x (DCons l' x' r) = let t = DCons l x (insertAt' t x' r) in t
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt DNil _ = error "Empty list"
+removeAt (DCons _ _ DNil) n
+    | n == 0 = DNil
+    | otherwise = error "Index out of bounds"
+removeAt (DCons l _ (DCons _ x r)) 0 = DCons l x r
+removeAt (DCons l x r) n = DCons l x (removeAt r (n - 1))
+
--- Task5_2.hs
+++ Task5_2.hs
@@ -1,13 +1,20 @@
 module Task5_2 where
 
 import Todo(todo)
-
 -- Зиппер из лекции 
 
 data Zipper a = Zipper [a] [a]
 
 -- Реализуйте экземпляры классов Show и Eq для этого типа
 
+instance Show a => Show (Zipper a) where
+    show (Zipper l r) = show l ++ "\n<..>\n" ++ show r
+    
+instance Eq a => Eq (Zipper a) where
+    (Zipper [] x) ==  (Zipper [] y) = x == y
+    (Zipper x []) ==  (Zipper y []) = x == y
+    z1@(Zipper l1 r1) == z2@(Zipper l2 r2) = l1 == l2 && r1 == r2 || goLeft z1 == goLeft z2
+ 
 fromList :: [a] -> Zipper a
 fromList lst = Zipper [] lst
 
@@ -31,14 +38,54 @@ removeRight (Zipper l (_:rt)) = Zipper l rt
 removeLeft :: Zipper a -> Zipper a
 removeLeft (Zipper (_:lt) r) = Zipper lt r
 
+
+-- my additional functions
+-- проверка, что все элементы зиппера находятся с одной стороны
+leftSided :: Zipper a -> Bool
+leftSided (Zipper _ []) = True
+leftSided _ = False
+
+rightSided :: Zipper a -> Bool
+rightSided (Zipper [] _) = True
+rightSided _ = False
+
+--перекидываем элементы зиппера в одну сторону
+allToRight :: Zipper a -> Zipper a
+allToRight z = if rightSided z then z else allToRight (goLeft z)
+  
+allToLeft :: Zipper a -> Zipper a
+allToLeft z = if leftSided z then z else allToLeft (goRight z)
+
+--обрезаем зиппер по одной стороне
+cutRight :: Zipper a -> Zipper a
+cutRight (Zipper l _) = Zipper l []
+
+cutLeft :: Zipper a -> Zipper a
+cutLeft (Zipper _ r) = Zipper [] r
 -- Используя приведённые выше функции, реализуйте функцию конкатенации
 -- вставки подсписка в середину и выделения подсписка
 
+
+-- не будем по одному перетаскивать значения из одного зиппера в другой
+--
+-- перекинув содержимое одного зиппера влево, другого - вправо,
+-- сольём их в один
 concat :: Zipper a -> Zipper a -> Zipper a
-concat left right = todo
+concat left right = merge (allToLeft left) (allToRight right)
+    where merge (Zipper l []) (Zipper [] r) = Zipper l r
+
+(<+>) = Task5_2.concat
 
 insertManyAt :: Int -> Zipper a -> Zipper a -> Zipper a
-insertManyAt index what into = todo
+insertManyAt index what into
+    | not $ rightSided into = insertManyAt index (allToRight what) (allToRight into)
+    | index == 0 = cutRight into <+> what <+> cutLeft into
+    | otherwise = insertManyAt (index - 1) what (goRight into)
 
+
+-- выделяемый кусок перемещаем влево, затем отрезаем правую часть
+-- предполагается, что нулевой индекс имеет голова правой части
 subZipper :: Int -> Int -> Zipper a -> Zipper a
-subZipper from to input = todo
+subZipper from to input
+    | to > 0 = subZipper (from - 1) (to - 1) (goRight input)
+    | otherwise = cutRight input
